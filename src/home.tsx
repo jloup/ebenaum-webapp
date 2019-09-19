5f771704d304e493988006310d3177659d251920
JLoup <jeanloup.jamet@gmail.com>
2019-09-19 17:27:56 +0200

    update

--- src/home.tsx
+++ src/home.tsx
@@ -54,7 +54,7 @@ export class Manifest extends React.Component<any, any> {
           <div className='col-8'>
             <div className='title'>Manifeste</div>
             <p>
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              {App.c('manifeste')}
             </p> 
           </div>
         </div> 
